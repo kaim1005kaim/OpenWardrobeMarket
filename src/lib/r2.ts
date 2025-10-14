5d81b83e03589cb0981023b3c3056823959eeffc
kaim1005kaim <kai.moriguchi1005@gmail.com>
2025-10-15 00:57:30 +0900

    feat(r2): Use custom domain for presigned URL endpoint

--- src/lib/r2.ts
+++ src/lib/r2.ts
@@ -7,7 +7,7 @@ import {
 } from "@aws-sdk/client-s3";
 import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
 
-const R2_ENDPOINT = process.env.R2_S3_ENDPOINT;
+const R2_ENDPOINT = process.env.R2_CUSTOM_DOMAIN_URL || process.env.R2_S3_ENDPOINT;
 const R2_ACCESS_KEY_ID = process.env.R2_ACCESS_KEY_ID;
 const R2_SECRET_ACCESS_KEY = process.env.R2_SECRET_ACCESS_KEY;
 const R2_BUCKET_ENV = process.env.R2_BUCKET;
